7bcbe962bc805fcf657519d77caa57fcca3b7d6f
Danila Kryukov <danila.kryukov.00@yandex.ru>
2023-09-27 14:58:17 +0700

    add smart pointer

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <vector>
 #include <random>
 #include <iostream>
+#include <memory>
 #define _USE_MATH_DEFINES 
 #include <math.h> // since we use cpp<20
 #include "curve.h"
 
-Curve* generate_random(){
+std::unique_ptr<Curve> generate_random(){
         static std::random_device rd;
         static std::mt19937 gen(rd());
         static std::uniform_real_distribution<> dis(1.0,16.0);
         int i = static_cast<int>(dis(gen)) % 3;
         switch(i){
                 case 0:
-                        return new Circle(dis(gen));
+                        return std::make_unique<Circle>(dis(gen));
+			// TODO or better: std::unique_ptr<Curve>(new Circle(dis(gen))); ?
                 case 1:
-                        return new Ellipse(dis(gen),dis(gen));
+                        return std::make_unique<Ellipse>(dis(gen),dis(gen));
                 case 2:
-                        return new Helix(dis(gen),dis(gen));
+                        return std::make_unique<Helix>(dis(gen),dis(gen));
         }
-        return nullptr;
+        return std::unique_ptr<Curve>(nullptr); // never go here
 }
 
 int main(void){
 	auto t = M_PI / 4;
-	std::vector<Curve*> curves(128); // TODO smart pointer
-	for(auto curve : curves){
+	std::vector<std::unique_ptr<Curve>> curves(128);
+	for(auto& curve : curves){
 		curve = generate_random();
 		// TODO better format
  		std::cout << "coord: " << curve->point(t).x
