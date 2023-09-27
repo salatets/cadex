56217b710e2c6d1c4f9937786efe848a25b41ebe
Danila Kryukov <danila.kryukov.00@yandex.ru>
2023-09-27 19:20:47 +0700

    add share_ptr implementation

--- main.cpp
+++ main.cpp
@@ -8,31 +8,43 @@
 #include <math.h> // since we use cpp<20
 #include "curve.ipp"
 
-std::unique_ptr<Curve<>> generate_random(){
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        static std::uniform_real_distribution<> dis(1.0,16.0);
+
+template<bool isShared>
+auto generate_random() -> typename std::conditional_t<
+				isShared,
+				std::shared_ptr<Curve<>>,
+				std::unique_ptr<Curve<>>>
+{
+	static std::random_device rd;
+	static std::mt19937 gen(rd());
+	static std::uniform_real_distribution<> dis(1.0,16.0);
+
         int i = static_cast<int>(dis(gen)) % 3;
         switch(i){
                 case 0:
+			if constexpr (isShared)
+                        return std::make_shared<Circle<>>(dis(gen));
+			else
                         return std::make_unique<Circle<>>(dis(gen));
-			// TODO or better: std::unique_ptr<Curve<>>(new Circle<>(dis(gen))); ?
                 case 1:
+			if constexpr (isShared)
+                        return std::make_shared<Ellipse<>>(dis(gen),dis(gen));
+			else
                         return std::make_unique<Ellipse<>>(dis(gen),dis(gen));
                 case 2:
+			if constexpr (isShared)
+                        return std::make_shared<Helix<>>(dis(gen),dis(gen));
+			else
                         return std::make_unique<Helix<>>(dis(gen),dis(gen));
         }
-        return std::unique_ptr<Curve<>>(nullptr); // never go here
+	// never go here
+	if constexpr (isShared)
+        return std::shared_ptr<Curve<>>(nullptr);
+	else
+        return std::unique_ptr<Curve<>>(nullptr);
 }
 
-int main(void){ // TODO add variant implementation with shared_ptr
-	auto t = M_PI / 4;
-	std::vector<std::unique_ptr<Curve<>>> curves(128);
-	std::vector<std::unique_ptr<Circle<>>*> circles; // If we not need autonomous collection
-
-	for(auto& curve : curves){
-		curve = generate_random();
-		// TODO better format
+void print_curve(Curve<>* const curve, double t){
  		std::cout << "coord: " << curve->point(t).x
                         << "," << curve->point(t).y
                         << "," << curve->point(t).z
@@ -40,13 +52,32 @@ int main(void){ // TODO add variant implementation with shared_ptr
                         << "," << curve->derivative(t).y
                         << "," << curve->derivative(t).z
                         << "\n";
-		if(dynamic_cast<Circle<>*>(curve.get())){
-                        circles.push_back(reinterpret_cast<std::unique_ptr<Circle<>>*>(&curve));
-                }
+}
+
+// TODO make dereferences consistant(without if constexpr)
+template<bool isShared, typename T, typename U>
+void test2(std::vector<T>&& curves, std::vector<U>&& circles){
+	auto t = M_PI / 4;
+	for(auto& curve : curves){
+		curve = generate_random<false>();
+		print_curve(curve.get(),t);
+		if constexpr (isShared){
+			if(auto ptr = std::dynamic_pointer_cast<Circle<>>(curve);ptr){
+				circles.push_back(ptr);
+			}
+		}else{
+			if(dynamic_cast<Circle<>*>(curve.get())){
+				circles.push_back(reinterpret_cast<std::unique_ptr<Circle<>>*>(&curve));
+			}
+		}
 	}
 	std::sort(circles.begin(), circles.end(),
                         [](auto const r, auto const l)
-                        {return (**r) < (**l);});
+                        {if constexpr (isShared){
+				return (*r) < (*l);
+			}else{
+				return (**r) < (**l);
+			}});
 
 	double sum = std::transform_reduce( // parallel computation using Standart Library
                         std::execution::par,
@@ -55,8 +86,11 @@ int main(void){ // TODO add variant implementation with shared_ptr
                         0.0,
                         std::plus{},
                         [](auto circle) {
-                                return (*circle)->radius(); }
-			); // is require additional allocations?
+			if constexpr (isShared){
+                                return circle->radius();
+			}else{
+                                return (*circle)->radius();
+			}}); // is require additional allocations?
 
 	double sumMP = 0.0;
         auto c_size = circles.size();
@@ -64,9 +98,34 @@ int main(void){ // TODO add variant implementation with shared_ptr
         // more overhead compare to manual critical section
         #pragma omp parallel for reduction (+:sumMP)
         for(auto i = 0; i < c_size; i++){
-                sumMP += (*circles[i])->radius();
+		if constexpr (isShared){
+			sumMP += circles[i]->radius();
+		}else{
+			sumMP += (*circles[i])->radius();
+		}
         }
 
 	std::cout << "sum of all circles: " << sum << " or with openMP: " << sumMP << "\n";
+}
+
+template<bool isShared>
+void test(){
+	if constexpr (isShared){
+		test2<isShared>
+			(std::vector<std::shared_ptr<Curve<>>>(128),
+			 std::vector<std::shared_ptr<Circle<>>>()
+			 );
+	}else{
+		test2<isShared>
+			(std::vector<std::unique_ptr<Curve<>>>(128),
+			 std::vector<std::unique_ptr<Circle<>>*>() // If we not need autonomous collection
+			);
+	}
+}
+
+
+int main(void){
+	test<true>();
+	test<false>();
 	return 0;
 }
